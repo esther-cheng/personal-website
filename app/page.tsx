0bff1d6f19be563ad8103683c166e3c044ffce35
Esther Cheng <ezc@duke.edu>
2025-12-18 20:04:26 -0500

    fixing css

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <Header activeLink="home" />
       <div className="home-page-container">
         <div className="left">
-          <img
-            src="/img/emphasis.png"
-            alt="emphasis lines"
-            className="emphasis-lines"
-          />
           <div className="welcome-container">
             <h1 className="welcome-title">Hey! I'm Esther.</h1>
+            {/* <img
+              src="/img/emphasis.png"
+              alt="emphasis lines"
+              className="emphasis-lines"
+            /> */}
             <h2 className="welcome-subtitle">Welcome to my page.</h2>
             <p className="welcome-blurb">
               I'm a Computer Science and Psychology student at Duke University.
